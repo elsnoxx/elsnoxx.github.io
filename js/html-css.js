f7c6221d0e3e8439c79af2ef7572a159bcbabb9e
elsnoxx <ficek.risa@gmail.com>
2023-07-21 14:17:22 +0200

    update

--- js/html-css.js
+++ js/html-css.js
@@ -28,4 +28,32 @@ $(document).ready(function() {
         $(".odpoved-html").toggle();
         console.log("html open");
     });
+    $(".meta").click(function(){
+        $(".odpoved-meta").toggle();
+        console.log("meta open");
+    });
+    $(".title").click(function(){
+        $(".odpoved-title").toggle();
+        console.log("title open");
+    });
+    $(".style").click(function(){
+        $(".odpoved-style").toggle();
+        console.log("style open");
+    });
+    $(".div").click(function(){
+        $(".odpoved-div").toggle();
+        console.log("div open");
+    });
+    $(".h").click(function(){
+        $(".odpoved-h").toggle();
+        console.log("h open");
+    });
+    $(".p").click(function(){
+        $(".odpoved-p").toggle();
+        console.log("p open");
+    });
+    $(".a").click(function(){
+        $(".odpoved-a").toggle();
+        console.log("a open");
+    });
 });
