e8e184113090d5efdfdd47fed349bdfa256bf068
elsnoxx <ficek.risa@gmail.com>
2023-07-21 09:22:49 +0200

    test

--- js/JQuery-general.js
+++ js/JQuery-general.js
@@ -1,16 +1,20 @@
 // Počkáme, až se načte celý dokument
 $(document).ready(function() {
     // Nastavíme akci po kliknutí na obrázek
-    $(".modal-img").on("click", function() {
+    $(".myImg").on("click", function() {
       // Zobrazíme modální okno
-      $("#modal").css("display", "block");
+      var altText = $(this).attr("alt");
+      $(".modal").css("display", "block");
       // Nastavíme zdroj obrázku v modálním okně na ten, který byl kliknutý
       $("#img01").attr("src", $(this).attr("src"));
+      $("#img01").attr("alt", altText);
+      console.log(altText)
+      $("#caption").text(altText);
     });
 
     // Akce po kliknutí na zavírací ikonu
     $(".close").on("click", function() {
       // Skryjeme modální okno
-      $("#modal").css("display", "none");
+      $(".modal").css("display", "none");
     });
   });
