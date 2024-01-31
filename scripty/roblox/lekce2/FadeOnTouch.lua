34c8be6230b54ea7a4c567985667ddde2776e997
elsnoxx <ficek.risa@gmail.com>
2024-01-31 06:22:38 +0100

    lekce2

---
+++ scripty/roblox/lekce2/FadeOnTouch.lua
@@ -0,0 +1,19 @@
+local platform = script.Parent
+local dotyk = false
+
+local function zmizeni()
+    if not dotyk then
+        dotyk = true
+        for count = 1, 10 do
+            platform.Transparency = count / 10
+            wait(0.1)
+        end
+        platform.CanCollide = false
+        wait(3)
+        platform.CanCollide = true
+        platform.Transparency = 0
+        isTouched = false
+    end
+end
+
+platform.Touches:Connect(Fade)
