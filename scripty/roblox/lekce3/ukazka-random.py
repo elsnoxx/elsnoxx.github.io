2df1e4557d39018fdc9399f8b0d1f4d9a9c46697
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-03-25 12:49:55 +0100

    uprava lekce 3

---
+++ scripty/roblox/lekce3/ukazka-random.py
@@ -0,0 +1,19 @@
+import random
+
+
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+
+
+nahodne_cislo = random.randint(0, 10)
+print("Random cislo 1: ",nahodne_cislo)
+nahodne_cislo = random.randint(0, 10)
+print("Random cislo 2: ",nahodne_cislo)
+
+
+print(" ")
+print(" ")
+print(" ")
+print(" ")
