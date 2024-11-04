7506cb5b00cb954be4b025c26ff46ad231b54762
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-11-04 11:09:46 +0100

    lekce 4 done

---
+++ js/javascript/lekce4.js
@@ -0,0 +1,25 @@
+
+function countRabbits() {
+    for (var i = 1; i <= 3; i++) {
+        alert("Králík číslo " + i);
+    }
+}
+
+
+function onButtonColorcChange() {
+    var text = document.getElementById('text');
+    var r = Math.floor(Math.random() * 250);
+    var g = Math.floor(Math.random() * 250);
+    var b = Math.floor(Math.random() * 250);
+    text.style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function onButtonCalc() {
+    var aEl = document.getElementById('a');
+    var bEl = document.getElementById('b');
+    var cEl = document.getElementById('c');
+    var a = Number(aEl.value);
+    var b = Number(bEl.value);
+    var c = a + b;
+    cEl.value = c;
+}
