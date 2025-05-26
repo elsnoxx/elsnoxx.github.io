fe4cd1c00667319d6568e947986016ededbb25d4
Richard Ficek <ficek.risa@gmail.com>
2025-05-26 14:49:58 +0200

    rozsireni ukolu

--- lessons/python_web/pracovni listy/lekce9/druhacast/mujprojekt/mojeaplikace/views.py
+++ lessons/python_web/pracovni listy/lekce9/druhacast/mujprojekt/mojeaplikace/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    message = ""
+    if request.method == "POST":
+        jmeno = request.POST.get("jmeno")
+        zprava = request.POST.get("zprava")
+        message = f"Děkujeme, {jmeno}, tvoje zpráva byla odeslána!"
+    return render(request, 'home.html', {"message": message})
